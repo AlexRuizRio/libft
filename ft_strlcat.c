9daef2bb18d69e27d2eccc9b92a52a78796f1ea7
Alejandro Ruiz Del Rio <alruiz-d@c2r11s3.42madrid.com>
2024-03-11 20:38:01 +0100

    Agregados los primeros 8 proyectos, el 12 y el 17. El atoi hay que modificarlo

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alruiz-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 15:42:22 by alruiz-d          #+#    #+#             */
+/*   Updated: 2024/03/11 15:42:25 by alruiz-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	len;
+
+	i = 0;
+	j = 0;
+	len = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[len] != '\0')
+		len++;
+	if (size <= i)
+		len += size;
+	else
+		len += i;
+	while (src[j] != '\0' && (i + 1) < size)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (len);
+}
