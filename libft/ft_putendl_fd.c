2eccb1ce09e9f2bd221467eb52eddc5a55e4139f
AlexRuizRio <alejandro.ruizrio@gmail.com>
2024-03-17 23:03:38 +0100

    Agregacion de 3 funciones y modificacion del libft

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -10,7 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	ft_putendl_fd(char *s, int fd)
 {
+    size_t	len;
 
+	len = ft_strlen;
+	write(fd, *s, len);
+    write(fd, "\n", 1);
 }
--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -10,7 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	ft_putstr_fd(char *s, int fd)
 {
+    size_t	len;
 
+	len = ft_strlen;
+	write(fd, *s, len);
 }
